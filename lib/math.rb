4ccf0d5be6496846f8e0b9844dfc9b4508944cda
Justin Langlinais <justinclanglinais@gmail.com>
2020-03-31 21:40:15 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,22 +3,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  sub_result = (num1 - num2)
-  p sub_result
+  p (num1 - num2)
 end
 
 def division(num1, num2)
-
+  p (num1 / num2)
 end
 
 def multiplication(num1, num2)
-
+  p (num1 * num2)
 end
 
 def modulo(num1, num2)
-
+  p (num1 % num2)
 end
 
 def square_root(num)
-
+  
 end
